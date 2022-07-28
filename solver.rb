841d6b76bddd2c852591adc251882517021c62db
Bria222 <briantechologies.co.ke@gmail.com>
2022-07-28 16:26:11 +0300

    create factorial class for solver clss

--- solver.rb
+++ solver.rb
@@ -1,3 +1,12 @@
 class Solver
-  
+  def factorial(num)
+    return 'No negative integer. This is an exception.' if num.negative?
+
+    case num
+    when 0
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
 end
